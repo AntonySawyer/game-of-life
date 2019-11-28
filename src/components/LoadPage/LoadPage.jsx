177faa3af2e21855522d2fd49666b74e0db721a5
Antony <A.Krutenok@gmail.com>
2019-11-28 11:27:04 +0300

    style: changed styles for inputs and cells

--- src/components/LoadPage/LoadPage.jsx
+++ src/components/LoadPage/LoadPage.jsx
@@ -3,11 +3,12 @@ import './LoadPage.css';
 
 function LoadPage(props) {
   return (
-    <section>
-      <h3>Click to load</h3>
-      {props.saves.map(save => {
-        return <li key={save._id} id={save._id} onClick={props.loadGame}>{save.name}</li>
-      })}
+    <section className="listWrapper">
+      <ul>
+        {props.saves.map(save => {
+          return <li key={save._id} id={save._id} onClick={props.loadGame}>{save.name}</li>
+        })}
+      </ul>
     </section>
   );
 }
